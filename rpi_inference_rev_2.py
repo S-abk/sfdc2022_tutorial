87d540ea62523fcad8453e78fc34a6fb9d1f399d
S-abk <117982032+S-abk@users.noreply.github.com>
2023-10-01 00:35:12 +0100

    Update rpi_inference_rev_2.py

--- rpi_inference_rev_2.py
+++ rpi_inference_rev_2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import cv2
 import torch
 import numpy as np
@@ -14,13 +13,7 @@ led1.off()
 led2.off()
 
 # Initialize the Pi Camera
-try:
-    piCam = Picamera2()
-except Exception as e:
-    print(f"Failed to initialize Pi Camera: {e}")
-    exit(1)
-
-# Configure camera settings
+piCam = Picamera2()
 piCam.preview_configuration.main.size = (224, 224)
 piCam.preview_configuration.main.format = "RGB888"
 piCam.preview_configuration.align()
@@ -34,39 +27,43 @@ torch.no_grad()
 # Function to convert OpenCV image to PyTorch tensor
 def opencv_image_to_tensor(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.astype(np.float32) / 255.0  # normalize to [0,1]
-    img = torch.tensor(img).permute(2, 0, 1)  # C, H, W
-    img = img.unsqueeze(0)  # N, C, H, W
+    img = img.astype(np.float32) / 255.0
+    img = torch.tensor(img).permute(2, 0, 1)
+    img = img.unsqueeze(0)
+    
+    # Quantize the tensor to match the model (Assuming qint8 quantization)
+    scale = 0.1
+    zero_point = 128
+    img = torch.quantize_per_tensor(img, scale=scale, zero_point=zero_point, dtype=torch.qint8)
+
     return img
 
 # Main Loop
-try:
-    while True:
-        # Capture a frame
-        frame = piCam.capture_array()
-        
-        # Convert frame to PyTorch tensor and make a prediction
-        tensor_input = opencv_image_to_tensor(frame)
+while True:
+    frame = piCam.capture_array()
+
+    tensor_input = opencv_image_to_tensor(frame)
+    
+    try:
         output = model(tensor_input)
         prediction = output[0].max(0)[1].item()
-        
-        # Control LEDs based on the prediction
-        if prediction == 0:
-            led1.on()
-            led2.off()
-        elif prediction == 1:
-            led1.off()
-            led2.on()
-        
-        # Display the frame
-        cv2.imshow("Real-Time Prediction", frame)
-        
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-finally:
-    # Cleanup
-    cv2.destroyAllWindows()
-    piCam.stop()  # Assuming Picamera2 has a stop() method for releasing resources
-    led1.off()
-    led2.off()
+    except Exception as e:
+        print(f"Model inference failed: {e}")
+        continue
+
+    if prediction == 0:
+        led1.on()
+        led2.off()
+    elif prediction == 1:
+        led1.off()
+        led2.on()
+
+    cv2.imshow("Real-Time Prediction", frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+piCam.stop()
+led1.off()
+led2.off()
